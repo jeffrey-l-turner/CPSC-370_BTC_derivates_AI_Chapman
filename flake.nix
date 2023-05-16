5ba88ce71820bfebe6bb861d33458080bcfbd04a
jlt <1573697+jeffrey-l-turner@users.noreply.github.com>
2023-05-16 16:08:46 -0700

    testing to get devenv.sh working

---
+++ flake.nix
@@ -0,0 +1,66 @@
+{
+  description = "A development environment for a Rust project";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachSystem ["x86_64-linux" "x86_64-darwin" "aarch64-darwin"] (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+
+        rustEnv = pkgs.buildRustPackage rec {
+          pname = "rust-dev";
+          version = "1.0.0";
+
+          src = ./.;
+
+          nativeBuildInputs = with pkgs; [
+            pkgconfig
+            openssl
+            cmake
+            protobuf
+            which
+            zlib
+            Security
+          ];
+
+          buildInputs = with pkgs; [
+            pkgconfig
+            openssl
+            cmake
+            protobuf
+            which
+            zlib
+            Security
+          ];
+
+          doCheck = false;
+
+          doInstallCheck = false;
+
+          meta = with pkgs.lib; {
+            description = "Rust development environment";
+            license = licenses.mit;
+            platforms = platforms.unix;
+          };
+        };
+        neovim = pkgs.neovim;
+        rust-analyzer = pkgs.rust-analyzer;
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = [
+            rustEnv
+            neovim
+            rust-analyzer
+          ];
+
+          shellHook = ''
+            echo "Welcome to the Rust development environment!"
+          '';
+        };
+      });
+}
