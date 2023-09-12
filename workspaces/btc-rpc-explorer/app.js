8aa6d376ba2de4251ff7e04d1baec732343ee266
carsonmccue <carsonem2003@gmail.com>
2023-09-12 13:15:07 -0700

    Refactor app.js to use consistent indentation and quotation marks.

--- workspaces/btc-rpc-explorer/app.js
+++ workspaces/btc-rpc-explorer/app.js
@@ -1,31 +1,32 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
-  const [data, setData] = useState(null);
+	const [data, setData] = useState(null);
 
-  useEffect(() => {
-    axios.get('/api/data')
-      .then(response => {
-        setData(response.data);
-      })
-      .catch(error => {
-        console.error(error);
-      });
-  }, []);
+	useEffect(() => {
+		axios
+			.get("/api/data")
+			.then((response) => {
+				setData(response.data);
+			})
+			.catch((error) => {
+				console.error(error);
+			});
+	}, []);
 
-  return (
-    <div>
-      {data ? (
-        <div>
-          <h1>{data.title}</h1>
-          <p>{data.description}</p>
-        </div>
-      ) : (
-        <p>Loading...</p>
-      )}
-    </div>
-  );
+	return (
+		<div>
+			{data ? (
+				<div>
+					<h1>{data.title}</h1>
+					<p>{data.description}</p>
+				</div>
+			) : (
+				<p>Loading...</p>
+			)}
+		</div>
+	);
 }
 
 export default App;
