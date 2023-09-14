be35a990b30d93a58472f996fc1ed83fa12e0ebd
JadenSuh <jsuh@chapman.edu>
2023-09-14 14:21:15 -0700

    Deleting Message

--- public/app.js
+++
@@ -1,6 +0,0 @@
-fetch('https://blockchain.info/ticker')
-  .then(response => response.json())
-  .then(data => {
-    document.getElementById('price').textContent = 'USD: ' + data.USD.last;
-  })
-  .catch(error => console.error('Error:', error));
