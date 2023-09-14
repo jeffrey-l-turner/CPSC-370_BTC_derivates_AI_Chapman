aeb4c0aefd7951091a8a0de07e01da2fd87567d5
carsonmccue <carsonem2003@gmail.com>
2023-09-14 12:28:26 -0700

    dev

---
+++ workspaces/my-btc-explorer/src/routes/+page.server.ts
@@ -0,0 +1,12 @@
+import { error } from '@sveltejs/kit';
+import type { PageServerLoad } from './$types';
+
+export const load: PageServerLoad = async ({ params }) => {
+	//const post = await (params.slug);
+
+	if (post) {
+		return post;
+	}
+
+	throw error(404, 'Not found');
+};
