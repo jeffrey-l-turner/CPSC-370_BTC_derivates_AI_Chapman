58494d9bce26e50c83b995ec7e6c32eb6a3711cc
JadenSuh <jsuh@chapman.edu>
2023-09-14 12:39:07 -0700

    aider: Added a new function `getBalanceSummary` to fetch and log the balance summary for a given address.
    # Aider chat conversation:
    
    ASSISTANT: To add this functionality, we need to modify the `app.js` file. We will add a new function `getBalanceSummary` that will take an address as a parameter and fetch the balance summary from the blockchain.com API.
    
    Here are the changes:
    
    ```javascript
    workspaces\blockchain.com\public\app.js
    <<<<<<< HEAD
    fetch('https://blockchain.info/ticker')
      .then(response => response.json())
      .then(data => {
        document.getElementById('price').textContent = 'USD: ' + data.USD.last;
      })
      .catch(error => console.error('Error:', error));
    =======
    fetch('https://blockchain.info/ticker')
      .then(response => response.json())
      .then(data => {
        document.getElementById('price').textContent = 'USD: ' + data.USD.last;
      })
      .catch(error => console.error('Error:', error));
    
    function getBalanceSummary(address) {
      fetch(`https://blockchain.info/balance?active=${address}`)
        .then(response => response.json())
        .then(data => {
          console.log(`Balance for ${address}: ${data[address].final_balance}`);
        })
        .catch(error => console.error('Error:', error));
    }
    >>>>>>> updated
    ```
    
    This new function `getBalanceSummary` fetches the balance summary for a given address and logs it to the console. You can call this function with an address to get the balance summary.

--- workspaces/blockchain.com/public/app.js
+++ workspaces/blockchain.com/public/app.js
@@ -4,3 +4,12 @@ fetch('https://blockchain.info/ticker')
     document.getElementById('price').textContent = 'USD: ' + data.USD.last;
   })
   .catch(error => console.error('Error:', error));
+
+function getBalanceSummary(address) {
+  fetch(`https://blockchain.info/balance?active=${address}`)
+    .then(response => response.json())
+    .then(data => {
+      console.log(`Balance for ${address}: ${data[address].final_balance}`);
+    })
+    .catch(error => console.error('Error:', error));
+}
