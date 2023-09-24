5dfcd92b1297ca3068a8448dadef6f3b7e706858
jlt <1573697+jeffrey-l-turner@users.noreply.github.com>
2023-09-24 14:17:12 -0700

    Return placeholder data for trending tokens in the router.

--- workspaces/mySwap/src/tRPC/router.ts
+++ workspaces/mySwap/src/tRPC/router.ts
@@ -12,7 +12,42 @@ export const router = createRouter()
     input: TrendingTokensInput,
     resolve: async ({ input }) => {
       // Replace this with your actual logic to fetch trending tokens
-      // For now, it just returns the input as is
-      return input
+      // For now, it just returns a placeholder data
+      return {
+        topTokens: [
+          {
+            id: '1',
+            decimals: 18,
+            name: 'Token 1',
+            chain: input.chain,
+            standard: 'ERC20',
+            address: '0x...',
+            symbol: 'TOKEN1',
+            market: {
+              id: '1',
+              price: {
+                id: '1',
+                value: '1.00',
+                currency: 'USD',
+              },
+              pricePercentChange: {
+                id: '1',
+                value: '0.01',
+              },
+              volume24H: {
+                id: '1',
+                value: '1000.00',
+                currency: 'USD',
+              },
+            },
+            project: {
+              id: '1',
+              logoUrl: 'https://example.com/logo.png',
+              safetyLevel: 'SAFE',
+            },
+          },
+          // Add more tokens as needed
+        ],
+      }
     },
   })
