889f8ba4e7c034f7311fb9e28dfca6e93ce8a703
jlt <1573697+jeffrey-l-turner@users.noreply.github.com>
2023-09-27 16:04:30 -0700

    aider: Refactor test to use Ramda's `compose` function for a pipeline of operations.
    # Context:
    USER: Please use Ramda in the tests to demonstrate the capability in the `workspaces/mySwap/src/Header.test.tsx` with compose also.
    ASSISTANT: Sure, we can use Ramda's `compose` function to create a pipeline of operations for our test. Here's how we can modify the `Header.test.tsx` file to use Ramda:
    
    ```tsx
    workspaces/mySwap/src/Header.test.tsx
    <<<<<<< ORIGINAL
    import React from 'react';
    import { render, screen } from '@testing-library/react';
    import { Header } from './Header';
    
    test('renders logo', () => {
      render(<Header />);
      const logoElement = screen.getByAltText(/Uniswap logo/i);
      expect(logoElement).toBeInTheDocument();
    });
    =======
    import React from 'react';
    import { render, screen } from '@testing-library/react';
    import { compose } from 'ramda';
    import { Header } from './Header';
    
    test('renders logo', () => {
      const renderHeaderAndCheckLogo = compose(
        (logoElement) => expect(logoElement).toBeInTheDocument(),
        () => screen.getByAltText(/Uniswap logo/i),
        () => render(<Header />)
      );
    
      renderHeaderAndCheckLogo();
    });
    >>>>>>> UPDATED
    ```
    
    In this updated test, we're using Ramda's `compose` function to create a pipeline of operations. The `compose` function takes a series of functions and returns a new function that applies these functions from right to left. So in this case, we first render the `Header` component, then we get the logo element, and finally we check if the logo element is in the document.
    
    This is a simple example, but it demonstrates how you can use Ramda to create a pipeline of operations in your tests.

--- workspaces/mySwap/src/Header.test.tsx
+++ workspaces/mySwap/src/Header.test.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import { compose } from 'ramda';
 import { Header } from './Header';
 
 test('renders logo', () => {
-  render(<Header />);
-  const logoElement = screen.getByAltText(/Uniswap logo/i);
-  expect(logoElement).toBeInTheDocument();
+  const renderHeaderAndCheckLogo = compose(
+    (logoElement) => expect(logoElement).toBeInTheDocument(),
+    () => screen.getByAltText(/Uniswap logo/i),
+    () => render(<Header />)
+  );
+
+  renderHeaderAndCheckLogo();
 });
