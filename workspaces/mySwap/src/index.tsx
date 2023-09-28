f1ff914f43b62aabc3c3417370aadb71142ca879
jlt <1573697+jeffrey-l-turner@users.noreply.github.com>
2023-09-28 12:41:56 -0700

    aider: Switch from `ReactDOM.render` to `ReactDOM.createRoot` to resolve the warning.
    # Context:
    USER: Getting this error: """react-dom.development.js:86 Warning: ReactDOM.render is no longer supported in React 18. Use createRoot instead. Until you switch to the new API, your app will behave as if it's running React 17. Learn more: https://reactjs.org/link/switch-to-createroot"""
    ASSISTANT: To resolve this warning, you need to switch from `ReactDOM.render` to `ReactDOM.createRoot`. This is a new API introduced in React 18.
    
    Here are the steps to make the change:
    
    1. Import `createRoot` from `react-dom` instead of `ReactDOM`.
    2. Use `createRoot` to render the `App` component.
    
    Here is the change you need to make in the `workspaces/mySwap/src/index.tsx` file:
    
    ```tsx
    workspaces/mySwap/src/index.tsx
    <<<<<<< ORIGINAL
    import React from 'react';
    import ReactDOM from 'react-dom';
    import App from './App.tsx';
    
    ReactDOM.render(
      <React.StrictMode>
        <App />
      </React.StrictMode>,
      document.getElementById('root')
    );
    =======
    import React from 'react';
    import { createRoot } from 'react-dom';
    import App from './App.tsx';
    
    createRoot(document.getElementById('root')).render(
      <React.StrictMode>
        <App />
      </React.StrictMode>
    );
    >>>>>>> UPDATED
    ```
    
    This change will switch your application to use the new `createRoot` API, and the warning should no longer appear.

--- workspaces/mySwap/src/index.tsx
+++ workspaces/mySwap/src/index.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom';
 import App from './App.tsx';
 
-ReactDOM.render(
+createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
