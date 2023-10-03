9dafbb64efb954df585c867d53926a8847285b6c
Noah Fuery <nfuery@chapman.edu>
2023-10-03 11:59:39 -0700

    Fixes

--- workspaces/21SeptAssignment/transaction.py
+++
@@ -1,8 +0,0 @@
-class Transaction:
-    def __init__(self):
-        # Initialize transaction history
-        self.transactions = []
-
-    def view_transactions(self):
-        for i, transaction in enumerate(self.transactions, 1):
-            print(f"Transaction {i}: {transaction}")
--- workspaces/mySwap/src/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { createRoot } from 'react-dom';
-import App from './App.tsx';
-
-createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
