6e79a2f5514afa135baf26b8e56b1d12eceaf6e3
jlt <1573697+jeffrey-l-turner@users.noreply.github.com>
2023-10-05 09:46:01 -0700

    aider: Adjusted the `checkForNewBlocks` interval function to terminate if 7 new blocks have been produced by `Blockchain1` and/or `Blockchain2`.
    # Context:
    USER: change the `InterBlockchain` code to terminate after either one minute or if 7 new blocks have been produced by `Blockchain1` and/or `Blockchain2`
    ASSISTANT: To implement this change, we need to modify the `interblockchain.ts` file. We will adjust the `checkForNewBlocks` interval function to terminate if 7 new blocks have been produced by `Blockchain1` and/or `Blockchain2`.
    
    Here is the change:
    
    ```typescript
    workspaces/cross-chain_composition/interblockchain.ts
    <<<<<<< ORIGINAL
        const checkForNewBlocks = setInterval(() => {
            const blocks1 = fs.readFileSync(logFile1, 'utf-8').split('\n').filter(Boolean).map(JSON.parse);
            const blocks2 = fs.readFileSync(logFile2, 'utf-8').split('\n').filter(Boolean).map(JSON.parse);
    
            if (blocks1.length + blocks2.length >= 10) {
                console.log('10 blocks produced. Terminating...');
                clearInterval(checkForNewBlocks);
                process.exit(0);
            } else {
                const currentTime = Date.now();
                if (lastBlockTime) {
                    const interval = currentTime - lastBlockTime;
                    console.log(`Time interval between blocks: ${interval} ms`);
                }
                lastBlockTime = currentTime;
    
                console.log('Shared State:', sharedState);
            }
        }, 3000); // Check every 3 seconds
        setTimeout(() => {
            console.log('One minute passed. Terminating...');
            clearInterval(checkForNewBlocks);
            process.exit(0);
        }, 60000); // Terminate after one minute
    =======
        const checkForNewBlocks = setInterval(() => {
            const blocks1 = fs.readFileSync(logFile1, 'utf-8').split('\n').filter(Boolean).map(JSON.parse);
            const blocks2 = fs.readFileSync(logFile2, 'utf-8').split('\n').filter(Boolean).map(JSON.parse);
    
            if (blocks1.length + blocks2.length - chain1Length - chain2Length >= 7) {
                console.log('7 new blocks produced. Terminating...');
                clearInterval(checkForNewBlocks);
                process.exit(0);
            } else {
                const currentTime = Date.now();
                if (lastBlockTime) {
                    const interval = currentTime - lastBlockTime;
                    console.log(`Time interval between blocks: ${interval} ms`);
                }
                lastBlockTime = currentTime;
    
                console.log('Shared State:', sharedState);
            }
        }, 3000); // Check every 3 seconds
        setTimeout(() => {
            console.log('One minute passed. Terminating...');
            clearInterval(checkForNewBlocks);
            process.exit(0);
        }, 60000); // Terminate after one minute
    >>>>>>> UPDATED
    ```
    
    This change will make the `InterBlockchain` function terminate after either one minute or if 7 new blocks have been produced by `Blockchain1` and/or `Blockchain2`.

--- workspaces/cross-chain_composition/interblockchain.ts
+++ workspaces/cross-chain_composition/interblockchain.ts
@@ -16,32 +16,30 @@ const logFile2 = 'blockchain2.log';
 let chain1Length = fs.readFileSync(logFile1, 'utf-8').split('\n').filter(Boolean).length;
 let chain2Length = fs.readFileSync(logFile2, 'utf-8').split('\n').filter(Boolean).length;
 
-const checkForNewBlocks = setInterval(() => {
-    const blocks1 = fs.readFileSync(logFile1, 'utf-8').split('\n').filter(Boolean).map(JSON.parse);
-    const blocks2 = fs.readFileSync(logFile2, 'utf-8').split('\n').filter(Boolean).map(JSON.parse);
+    const checkForNewBlocks = setInterval(() => {
+        const blocks1 = fs.readFileSync(logFile1, 'utf-8').split('\n').filter(Boolean).map(JSON.parse);
+        const blocks2 = fs.readFileSync(logFile2, 'utf-8').split('\n').filter(Boolean).map(JSON.parse);
 
-    if (blocks1.length + blocks2.length >= 10) {
-        console.log('10 blocks produced. Terminating...');
-        clearInterval(checkForNewBlocks);
-        process.exit(0);
-    } else {
-        const currentTime = Date.now();
-        if (lastBlockTime) {
-            const interval = currentTime - lastBlockTime;
-            console.log(`Time interval between blocks: ${interval} ms`);
+        if (blocks1.length + blocks2.length - chain1Length - chain2Length >= 7) {
+            console.log('7 new blocks produced. Terminating...');
+            clearInterval(checkForNewBlocks);
+            process.exit(0);
+        } else {
+            const currentTime = Date.now();
+            if (lastBlockTime) {
+                const interval = currentTime - lastBlockTime;
+                console.log(`Time interval between blocks: ${interval} ms`);
+            }
+            lastBlockTime = currentTime;
+
+            console.log('Shared State:', sharedState);
         }
-        lastBlockTime = currentTime;
-
-
-        console.log('Shared State:', sharedState);
-    }
-}, 3000); // Check every 3 seconds
+    }, 3000); // Check every 3 seconds
     setTimeout(() => {
         console.log('One minute passed. Terminating...');
         clearInterval(checkForNewBlocks);
         process.exit(0);
     }, 60000); // Terminate after one minute
-
     // Removed direct calls to createBlock method of both blockchains
     // Removed logs of the blockchain chains and shared state
 
