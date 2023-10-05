70058606ae7cff0032ef34af7cdd691532fbe417
jlt <1573697+jeffrey-l-turner@users.noreply.github.com>
2023-10-05 08:24:07 -0700

    aider: Updated `Blockchain1` to maintain a chain of blocks with index, timestamp, data, previous hash, and hash, and added functions to create blocks and calculate hashes.
    # Context:
    USER: Implement a linked list of immutable states in Blockchain1 with hash functions to demonstrate the concepts.
    ASSISTANT: To implement a linked list of immutable states in `Blockchain1` with hash functions, we will modify the `blockchain1.ts` file.
    
    Here are the changes:
    
    ```typescript
    workspaces/cross-chain_composition/blockchain1.ts
    <<<<<<< ORIGINAL
    const Blockchain1 = () => {
        // Blockchain1 implementation goes here
    }
    export default Blockchain1;
    =======
    import crypto from 'crypto';
    
    const Blockchain1 = () => {
        let chain = [];
    
        const createBlock = (data) => {
            const previousBlock = chain[chain.length - 1];
            const block = {
                index: chain.length,
                timestamp: Date.now(),
                data,
                previousHash: previousBlock ? previousBlock.hash : null,
                hash: ''
            };
            block.hash = calculateHash(block);
            chain.push(block);
        };
    
        const calculateHash = (block) => {
            const { index, timestamp, data, previousHash } = block;
            return crypto.createHash('sha256').update(index + timestamp + JSON.stringify(data) + previousHash).digest('hex');
        };
    
        // Create the genesis block
        createBlock({ info: 'Genesis Block' });
    
        return {
            chain,
            createBlock
        };
    }
    export default Blockchain1;
    >>>>>>> UPDATED
    ```
    
    In this updated code, `Blockchain1` now maintains a chain of blocks, each with an index, timestamp, data, previous hash, and hash. The `createBlock` function is used to create a new block and add it to the chain. The `calculateHash` function is used to calculate the hash of a block based on its index, timestamp, data, and previous hash. The genesis block is created when `Blockchain1` is initialized.

--- workspaces/cross-chain_composition/blockchain1.ts
+++ workspaces/cross-chain_composition/blockchain1.ts
@@ -1,4 +1,32 @@
+import crypto from 'crypto';
+
 const Blockchain1 = () => {
-    // Blockchain1 implementation goes here
+    let chain = [];
+
+    const createBlock = (data) => {
+        const previousBlock = chain[chain.length - 1];
+        const block = {
+            index: chain.length,
+            timestamp: Date.now(),
+            data,
+            previousHash: previousBlock ? previousBlock.hash : null,
+            hash: ''
+        };
+        block.hash = calculateHash(block);
+        chain.push(block);
+    };
+
+    const calculateHash = (block) => {
+        const { index, timestamp, data, previousHash } = block;
+        return crypto.createHash('sha256').update(index + timestamp + JSON.stringify(data) + previousHash).digest('hex');
+    };
+
+    // Create the genesis block
+    createBlock({ info: 'Genesis Block' });
+
+    return {
+        chain,
+        createBlock
+    };
 }
 export default Blockchain1;
