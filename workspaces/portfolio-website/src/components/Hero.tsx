c27df5055a32a1137a2b3d5cffbfce0190a43a9d
Gilberto Arellano Lopez <giarellano@chapman.edu>
2023-10-26 16:38:17 -0700

    Completed 3rd Assignment

---
+++ workspaces/portfolio-website/src/components/Hero.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Hero = () => {
+  return (
+    <section>
+      <h2>Welcome to my portfolio!</h2>
+      <p>I'm a software developer specializing in blockchain technology. I have experience in developing smart contracts, dApps, and blockchain-based solutions.</p>
+    </section>
+  );
+};
+
+export default Hero;
---
+++ workspaces/portfolio-website/src/components/Testimonials.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Testimonials = () => {
+  return (
+    <section>
+      <h2>Testimonials</h2>
+      <p>Here's what people are saying about my work:</p>
+      {/* Add your testimonials here */}
+    </section>
+  );
+};
+
+export default Testimonials;
