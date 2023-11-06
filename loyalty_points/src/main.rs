6e495fd6fc8d917dc3bd53f15918e9f34b129cd3
jackmazac <mazac@chapman.edu>
2023-11-06 14:19:39 -0800

    aider: Moved the `loyalty_points.rs` code into the `src/main.rs` file in the new `loyalty_points` project.
    # Aider chat conversation:
    
    ASSISTANT: Great! The `cargo new loyalty_points` command creates a new Rust project with the name `loyalty_points`. This project is a binary (application) package, which means it's intended to be a standalone program. The project will be created in a new directory named `loyalty_points`.
    
    The structure of the new project looks like this:
    
    ```
    loyalty_points
    ├── Cargo.toml
    └── src
        └── main.rs
    ```
    
    - `Cargo.toml` is the package manifest where you specify your package information and dependencies.
    - `src/main.rs` is where the main function of your program lives. This is the entry point of your application.
    
    You can now move the `loyalty_points.rs` code into the `src/main.rs` file in the new `loyalty_points` project.
    
    Let's do that with an *edit block*:
    
    ```rust
    loyalty_points/src/main.rs
    <<<<<<< HEAD
    
    =======
    use rgbstd::interface::{rgb20, ContractBuilder};
    
    use std::convert::Infallible;
    use std::fs;
    
    use amplify::hex::FromHex;
    use bp::{Chain, Outpoint, Tx, Txid};
    use rgb_schemata::{nia_rgb20, nia_schema};
    use rgbstd::containers::BindleContent;
    use rgbstd::contract::WitnessOrd;
    use rgbstd::resolvers::ResolveHeight;
    use rgbstd::stl::{
        Amount, ContractData, DivisibleAssetSpec, Precision, RicardianContract, Timestamp,
    };
    use rgbstd::validation::{ResolveTx, TxResolverError};
    use strict_encoding::StrictDumb;
    
    struct DumbResolver;
    
    impl ResolveTx for DumbResolver {
        fn resolve_tx(&self, _txid: Txid) -> Result<Tx, TxResolverError> {
            Ok(Tx::strict_dumb())
        }
    }
    
    impl ResolveHeight for DumbResolver {
        type Error = Infallible;
        fn resolve_height(&mut self, _txid: Txid) -> Result<WitnessOrd, Self::Error> {
            Ok(WitnessOrd::OffChain)
        }
    }
    
    fn main() {
        let name = "Loyalty Points";  // name of token
        let decimal = Precision::None; // Decimal: 0
        let desc = "Loyalty Points for XYZ Retail Store";  // token description
        let spec = DivisibleAssetSpec::with("RGB20", name, decimal, Some(desc)).unwrap();
        let terms = RicardianContract::default();
        let contract_data = ContractData { terms, media: None };
        let created = Timestamp::now();
        let txid = "9fef7f1c9cd1dd6b9ac5c0a050103ad874346d4fdb7bcf954de2dfe64dd2ce05";
        // token owner address in utxo
        let beneficiary = Outpoint::new(Txid::from_hex(txid).unwrap(), 0);
    
        const ISSUE: u64 = 10_000_000;  // Initial supply of loyalty points
    
        let contract = ContractBuilder::with(rgb20(), nia_schema(), nia_rgb20())
            .expect("schema fails to implement RGB20 interface")
            .set_chain(Chain::Testnet3)
            .add_global_state("spec", spec)
            .expect("invalid nominal")
            .add_global_state("created", created)
            .expect("invalid creation date")
            .add_global_state("issuedSupply", Amount::from(ISSUE))
            .expect("invalid issued supply")
            .add_global_state("data", contract_data)
            .expect("invalid contract text")
            .add_fungible_state("assetOwner", beneficiary, ISSUE)
            .expect("invalid asset amount")
            .issue_contract()
            .expect("contract doesn't fit schema requirements");
    
        let contract_id = contract.contract_id();
        debug_assert_eq!(contract_id, contract.contract_id());
    
        let bindle = contract.bindle();
        eprintln!("{bindle}");
        bindle.save("contracts/loyalty-points.contract.rgb")
            .expect("unable to save contract");
        fs::write("contracts/loyalty-points.contract.rgba", bindle.to_string())
            .expect("unable to save contract");
    }
    >>>>>>> updated
    ```
    
    After this, you should be able to build and run your project with `cargo build` and `cargo run` commands respectively.

--- loyalty_points/src/main.rs
+++ loyalty_points/src/main.rs
@@ -1,3 +1,75 @@
 fn main() {
     println!("Hello, world!");
 }
+use rgbstd::interface::{rgb20, ContractBuilder};
+
+use std::convert::Infallible;
+use std::fs;
+
+use amplify::hex::FromHex;
+use bp::{Chain, Outpoint, Tx, Txid};
+use rgb_schemata::{nia_rgb20, nia_schema};
+use rgbstd::containers::BindleContent;
+use rgbstd::contract::WitnessOrd;
+use rgbstd::resolvers::ResolveHeight;
+use rgbstd::stl::{
+    Amount, ContractData, DivisibleAssetSpec, Precision, RicardianContract, Timestamp,
+};
+use rgbstd::validation::{ResolveTx, TxResolverError};
+use strict_encoding::StrictDumb;
+
+struct DumbResolver;
+
+impl ResolveTx for DumbResolver {
+    fn resolve_tx(&self, _txid: Txid) -> Result<Tx, TxResolverError> {
+        Ok(Tx::strict_dumb())
+    }
+}
+
+impl ResolveHeight for DumbResolver {
+    type Error = Infallible;
+    fn resolve_height(&mut self, _txid: Txid) -> Result<WitnessOrd, Self::Error> {
+        Ok(WitnessOrd::OffChain)
+    }
+}
+
+fn main() {
+    let name = "Loyalty Points";  // name of token
+    let decimal = Precision::None; // Decimal: 0
+    let desc = "Loyalty Points for XYZ Retail Store";  // token description
+    let spec = DivisibleAssetSpec::with("RGB20", name, decimal, Some(desc)).unwrap();
+    let terms = RicardianContract::default();
+    let contract_data = ContractData { terms, media: None };
+    let created = Timestamp::now();
+    let txid = "9fef7f1c9cd1dd6b9ac5c0a050103ad874346d4fdb7bcf954de2dfe64dd2ce05";
+    // token owner address in utxo
+    let beneficiary = Outpoint::new(Txid::from_hex(txid).unwrap(), 0);
+
+    const ISSUE: u64 = 10_000_000;  // Initial supply of loyalty points
+
+    let contract = ContractBuilder::with(rgb20(), nia_schema(), nia_rgb20())
+        .expect("schema fails to implement RGB20 interface")
+        .set_chain(Chain::Testnet3)
+        .add_global_state("spec", spec)
+        .expect("invalid nominal")
+        .add_global_state("created", created)
+        .expect("invalid creation date")
+        .add_global_state("issuedSupply", Amount::from(ISSUE))
+        .expect("invalid issued supply")
+        .add_global_state("data", contract_data)
+        .expect("invalid contract text")
+        .add_fungible_state("assetOwner", beneficiary, ISSUE)
+        .expect("invalid asset amount")
+        .issue_contract()
+        .expect("contract doesn't fit schema requirements");
+
+    let contract_id = contract.contract_id();
+    debug_assert_eq!(contract_id, contract.contract_id());
+
+    let bindle = contract.bindle();
+    eprintln!("{bindle}");
+    bindle.save("contracts/loyalty-points.contract.rgb")
+        .expect("unable to save contract");
+    fs::write("contracts/loyalty-points.contract.rgba", bindle.to_string())
+        .expect("unable to save contract");
+}
