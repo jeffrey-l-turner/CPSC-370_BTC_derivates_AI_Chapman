3cb100558626768ac9f0f0877200aaac9697f884
devinng <112991428+dng28@users.noreply.github.com>
2023-11-14 12:16:20 -0800

    aider: Generated a new `schema.rs` file suitable for the dependencies listed in the `Cargo.toml` file.
    # Aider chat conversation:
    
    USER: Given the following contents in my Cargo.toml file, can you generate a brand new schema.rs file that is suitable for those contents: [package]
    name = "rgb20-token"
    version = "0.1.0"
    edition = "2021"
    
    [[bin]]
    name = "my_program"
    path = "src/main.rs"
    
    [lib]
    name = "rgb20_token"
    path = "src/lib.rs"
    
    [dependencies]
    amplify = "4.0.1"
    strict_encoding = "2.5.0"
    strict_types = "1.6.1"
    bp-core = "0.10.9"
    rgb-std = { version = "0.10.8", features = ["serde", "fs"] }
    serde = "1.0.152"
    serde_json = "1.0.93"
    rgb-schemata = "0.10.0"
    lazy_static = "1.4.0"
    system = "0.1.0"
    library = "0.0.2"
    some_crate = "0.1.0"
    
    ASSISTANT: To generate a new `schema.rs` file that is suitable for the dependencies listed in your `Cargo.toml` file, we need to ensure that the code in `schema.rs` is compatible with the versions of the crates you are using. Since you have `rgb-std` version `0.10.8`, we will create a `schema.rs` file that uses the API provided by that version of `rgb-std`.
    
    Here is the content for the new `schema.rs` file:
    
    ```rust
    workspaces/rgb20-token/src/schema.rs
    <<<<<<< SEARCH
    
    =======
    use amplify::Wrapper;
    use rgb_std::schema::{Constants, GenesisSchema, Schema, TransitionSchema};
    use rgb_std::type_map;
    use rgb_std::type_map::TypeMap;
    use rgb_std::prelude::*;
    use strict_encoding::{StrictDecode, StrictEncode};
    
    // Define your custom types and their properties
    #[derive(Clone, Debug, StrictEncode, StrictDecode)]
    pub struct MyCustomToken {
        ticker: String,
        name: String,
        description: String,
        total_supply: u64,
    }
    
    // Define the schema for an RGB20 token
    pub fn rgb20_schema() -> Schema {
        Schema {
            rgb_features: type_map! {},
            root_id: Default::default(),
            field_types: type_map! {
                // Define the fields for your token
                FieldType::AsciiString => DataFormat::String(256),
                FieldType::Unsigned64 => DataFormat::Unsigned(64, 0, u64::MAX), // Total supply
            },
            owned_right_types: type_map! {
                // Right to own tokens
                OwnedRightType::Inflation => TransitionSchema {
                    closes: None,
                    metadata: type_map! {},
                    defines: type_map! {
                        OwnedRightType::Inflation => StateSchema {
                            format: DataFormat::Unsigned(64, 0, u64::MAX), // Token balance
                            abi: type_map! {},
                        },
                    },
                    abi: type_map! {},
                },
            },
            public_right_types: type_map! {},
            genesis: GenesisSchema {
                metadata: type_map! {
                    FieldType::AsciiString => once!(b"Ticker"),
                    FieldType::AsciiString => once!(b"Name"),
                    FieldType::AsciiString => once!(b"Description"),
                    FieldType::Unsigned64 => once!(b"TotalSupply"),
                },
                owned_rights: type_map! {
                    OwnedRightType::Inflation => None,
                },
                public_rights: type_map! {},
                abi: type_map! {},
            },
            extensions: type_map! {},
            transitions: type_map! {},
            constants: Constants::default(),
        }
    }
    >>>>>>> REPLACE
    ```
    
    Please note that the above code is a template and may need to be adjusted to fit the actual API of the `rgb-std` crate version `0.10.8`. The `FieldType`, `OwnedRightType`, and other types and constants used in the code should be defined according to the `rgb-std` crate's documentation. If there are any specific fields or features you want to include in your schema, please let me know, and I can adjust the code accordingly.

--- workspaces/rgb20-token/src/schema.rs
+++ workspaces/rgb20-token/src/schema.rs
@@ -65,3 +65,62 @@ pub struct MyCustomToken {
         },
     }
 }
+use amplify::Wrapper;
+use rgb_std::schema::{Constants, GenesisSchema, Schema, TransitionSchema};
+use rgb_std::type_map;
+use rgb_std::type_map::TypeMap;
+use rgb_std::prelude::*;
+use strict_encoding::{StrictDecode, StrictEncode};
+
+// Define your custom types and their properties
+#[derive(Clone, Debug, StrictEncode, StrictDecode)]
+pub struct MyCustomToken {
+    ticker: String,
+    name: String,
+    description: String,
+    total_supply: u64,
+}
+
+// Define the schema for an RGB20 token
+pub fn rgb20_schema() -> Schema {
+    Schema {
+        rgb_features: type_map! {},
+        root_id: Default::default(),
+        field_types: type_map! {
+            // Define the fields for your token
+            FieldType::AsciiString => DataFormat::String(256),
+            FieldType::Unsigned64 => DataFormat::Unsigned(64, 0, u64::MAX), // Total supply
+        },
+        owned_right_types: type_map! {
+            // Right to own tokens
+            OwnedRightType::Inflation => TransitionSchema {
+                closes: None,
+                metadata: type_map! {},
+                defines: type_map! {
+                    OwnedRightType::Inflation => StateSchema {
+                        format: DataFormat::Unsigned(64, 0, u64::MAX), // Token balance
+                        abi: type_map! {},
+                    },
+                },
+                abi: type_map! {},
+            },
+        },
+        public_right_types: type_map! {},
+        genesis: GenesisSchema {
+            metadata: type_map! {
+                FieldType::AsciiString => once!(b"Ticker"),
+                FieldType::AsciiString => once!(b"Name"),
+                FieldType::AsciiString => once!(b"Description"),
+                FieldType::Unsigned64 => once!(b"TotalSupply"),
+            },
+            owned_rights: type_map! {
+                OwnedRightType::Inflation => None,
+            },
+            public_rights: type_map! {},
+            abi: type_map! {},
+        },
+        extensions: type_map! {},
+        transitions: type_map! {},
+        constants: Constants::default(),
+    }
+}
