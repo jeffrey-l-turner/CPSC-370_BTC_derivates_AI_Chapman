7914bc26c165a7d86f2a9402bfe10c1e38e815dc
Gilberto Arellano Lopez <giarellano@chapman.edu>
2023-12-01 13:29:34 -0800

    First iteration of profile setup

--- Dating-DApp/final-project/src/routes/protected/profile/+page.svelte
+++ Dating-DApp/final-project/src/routes/protected/profile/+page.svelte
@@ -8,10 +8,47 @@
 	Add in stytch data
 	Add in sveltekit actions to edit data
 -->
-<h1 class="text-2xl">My profile</h1>
+<h1 class="text-2xl">Profile Setup</h1>
+
+<h2 class="text-xl">John Smith</h2>
 
 <div class="space-y-3 mt-5">
-	<input type="file" class="file-input file-input-sm file-input-bordered w-full" />
 
-	<textarea class="textarea textarea-bordered w-full" placeholder="Bio"></textarea>
+	<div class="card w-96 bg-base-100 shadow-xl rounded-lg flex justify-center items-center">
+		<figure class="flex justify-center items-center text-center h-56 w-full bg-gray-200"> <!-- Adjust height using h-56 and background color using bg-gray-200 -->
+		  <div class="placeholder">
+			<span class="text-lg font-semibold text-gray-700">Upload Image</span> <!-- Added font-semibold and text-gray-700 for better visibility -->
+		  </div>
+		</figure>
+	</div>
+	  
+
+	<button class="btn">Confirm Age</button>
+	<button class="btn">Confirm Location</button>
+	
+	<label class="form-control w-full max-w-xs">
+		<div class="label">
+		  <span class="label-text">Height</span>
+		</div>
+		<input type="text" placeholder='ex: 5&#39; 2"' class="input input-bordered w-full max-w-xs" />
+	</label>
+
+
+	<label class="form-control w-full max-w-xs">
+		<div class="label">
+		  <span class="label-text">Bio:</span>
+		</div>
+		<input type="text" placeholder="Enter bio here..." class="input input-bordered w-full max-w-xs" />
+	</label>
+
+	<div class="card w-96 bg-base-100 shadow-xl rounded-lg flex justify-center items-center">
+		<figure class="flex justify-center items-center text-center h-56 w-full bg-gray-200"> <!-- Adjust height using h-56 and background color using bg-gray-200 -->
+		  <div class="placeholder">
+			<span class="text-lg font-semibold text-gray-700">Upload Image</span> <!-- Added font-semibold and text-gray-700 for better visibility -->
+		  </div>
+		</figure>
+	</div>
+	  
+
+
 </div>
