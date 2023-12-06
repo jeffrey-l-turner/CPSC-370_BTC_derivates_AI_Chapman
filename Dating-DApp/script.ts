183ce8dfe77a955e7166e5def55a572d41f6b679
Noah Fuery <nfuery@chapman.edu>
2023-12-06 15:08:19 -0800

    aider: Added a function to create a user with the provided parameters and called this function for each user in the provided list.
    # Aider chat conversation:
    
    ASSISTANT: To make this change we need to modify `Dating-DApp/script.ts` to:
    
    1. Add a function to create a user with the provided parameters.
    2. Call this function for each user in the provided list.
    
    Here is the *SEARCH/REPLACE* block:
    
    Dating-DApp/script.ts
    ```typescript
    <<<<<<< SEARCH
    async function main() {
      // ... you will write your Prisma Client queries here
    }
    =======
    async function createUser(username: string, email: string, password: string) {
      const user = await prisma.user.create({
        data: {
          username: username,
          email: email,
          password: password,
          profiles: {
            create: {
              firstName: username.split(' ')[0],
              lastName: username.split(' ')[1],
              dateOfBirth: new Date(),
              gender: 'Male',
              interestedIn: 'Female',
              bio: 'This is a bio',
              city: 'City',
              wallet: 'Wallet',
              country: 'Country',
            }
          },
          photos: {
            create: {
              photo: new Buffer('Photo'),
            }
          }
        },
      })
      console.log(user)
    }
    
    async function main() {
      const users = [
        'Ava Ambuske',
        'Gilberto Arellano',
        'Jesse Arevalo Baez',
        'Parker Escalette',
        'Noah Fuery',
        'Emma Gibson',
        'Alex Haberman',
        'Jack Mazac',
        'Carson McCue',
        'Katherine Monroy',
        'Chris Nam',
        'Devin Ng',
        'Luis Rivas',
        'Peter Senko',
        'James Shan',
        'Jaden Suh'
      ]
    
      for (const user of users) {
        await createUser(user, `${user.replace(' ', '').toLowerCase()}@prisma.io`, 'password')
      }
    }
    >>>>>>> REPLACE
    ```
    
    This code will create a user for each name in the list, using the name as the username, a generated email based on the name, and a static password. The profile and photo for each user are created with dummy data.

--- Dating-DApp/script.ts
+++ Dating-DApp/script.ts
@@ -2,8 +2,58 @@ import { PrismaClient } from '@prisma/client'
 
 const prisma = new PrismaClient()
 
+async function createUser(username: string, email: string, password: string) {
+  const user = await prisma.user.create({
+    data: {
+      username: username,
+      email: email,
+      password: password,
+      profiles: {
+        create: {
+          firstName: username.split(' ')[0],
+          lastName: username.split(' ')[1],
+          dateOfBirth: new Date(),
+          gender: 'Male',
+          interestedIn: 'Female',
+          bio: 'This is a bio',
+          city: 'City',
+          wallet: 'Wallet',
+          country: 'Country',
+        }
+      },
+      photos: {
+        create: {
+          photo: new Buffer('Photo'),
+        }
+      }
+    },
+  })
+  console.log(user)
+}
+
 async function main() {
-  // ... you will write your Prisma Client queries here
+  const users = [
+    'Ava Ambuske',
+    'Gilberto Arellano',
+    'Jesse Arevalo Baez',
+    'Parker Escalette',
+    'Noah Fuery',
+    'Emma Gibson',
+    'Alex Haberman',
+    'Jack Mazac',
+    'Carson McCue',
+    'Katherine Monroy',
+    'Chris Nam',
+    'Devin Ng',
+    'Luis Rivas',
+    'Peter Senko',
+    'James Shan',
+    'Jaden Suh'
+  ]
+
+  for (const user of users) {
+    await createUser(user, `${user.replace(' ', '').toLowerCase()}@prisma.io`, 'password')
+  }
 }
 
 main()
