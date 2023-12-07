66890af52b573a6cd0a4fe2c58c9648670094115
Noah Fuery <nfuery@chapman.edu>
2023-12-07 12:36:32 -0800

    aider: Refactored matchmaking functions to add potential matches and record accepted matches.
    # Aider chat conversation:
    
    USER: Here is an update to the matchmaking: data structures
    the database will contain for each user
    
    potentialMatches,
    acceptedMatches
    operations
    The frontend function that adds a person P to the database calls the function `
    
    addPotentialMatch,
    which goes through all persons P_i in the database and, if the hard constraints allows, adds P to the potentialMatches of P_i
    
    The frontend function that records that P likes P’ calls the function
    
    addAcceptedMatch
    which, if P’ also likes P, adds P to the acceptedMatches of P’ and adds P’ to the acceptedMatches of P. The users will like each other in order to initiate matches, if two users like each other, then they will go from potential matches into acceptedMatches
    
    ASSISTANT: To implement the changes you've described, we need to modify the `matchmaking.ts` file. Here are the steps:
    
    1. Remove the `makePotentialMatches` function and replace it with `addPotentialMatch` function. This function will add a new user to the potentialMatches of all existing users if the hard constraints allow it.
    2. Remove the `acceptMatch` function and replace it with `addAcceptedMatch` function. This function will add a user to the acceptedMatches of another user if they both like each other.
    
    Here are the *SEARCH/REPLACE* blocks:
    
    Dating-DApp/matchmaking.ts
    ```typescript
    <<<<<<< SEARCH
    async function makePotentialMatches(userId: string) {
      const user = await prisma.profile.findUnique({ where: { id: userId } });
      const potentialMatches = await prisma.profile.findMany({
        where: {
          gender: user.interestedIn,
          interestedIn: user.gender,
        },
      });
      return potentialMatches;
    }
    =======
    async function addPotentialMatch(newUserId: string) {
      const newUser = await prisma.profile.findUnique({ where: { id: newUserId } });
      const existingUsers = await prisma.profile.findMany();
    
      for (const user of existingUsers) {
        if (user.gender === newUser.interestedIn && user.interestedIn === newUser.gender) {
          // Add new user to the potentialMatches of the existing user
          // This assumes that potentialMatches is a field in the Profile model
          user.potentialMatches.push(newUserId);
          await prisma.profile.update({
            where: { id: user.id },
            data: { potentialMatches: user.potentialMatches },
          });
        }
      }
    }
    >>>>>>> REPLACE
    ```
    
    Dating-DApp/matchmaking.ts
    ```typescript
    <<<<<<< SEARCH
    async function acceptMatch(userId: string, requestedMatchId: string) {
      const acceptedMatch = await prisma.like.create({
        data: {
          userId: userId,
          acceptedMatchId: requestedMatchId,
        },
      });
      return acceptedMatch;
    }
    =======
    async function addAcceptedMatch(userId: string, likedUserId: string) {
      const user = await prisma.profile.findUnique({ where: { id: userId } });
      const likedUser = await prisma.profile.findUnique({ where: { id: likedUserId } });
    
      if (user.potentialMatches.includes(likedUserId) && likedUser.potentialMatches.includes(userId)) {
        // Add each other to the acceptedMatches
        // This assumes that acceptedMatches is a field in the Profile model
        user.acceptedMatches.push(likedUserId);
        likedUser.acceptedMatches.push(userId);
    
        await prisma.profile.update({
          where: { id: userId },
          data: { acceptedMatches: user.acceptedMatches },
        });
    
        await prisma.profile.update({
          where: { id: likedUserId },
          data: { acceptedMatches: likedUser.acceptedMatches },
        });
      }
    }
    >>>>>>> REPLACE
    ```
    
    Please note that this assumes that `potentialMatches` and `acceptedMatches` are fields in the `Profile` model in `schema.prisma`. If they are not, you will need to add them.

--- Dating-DApp/matchmaking.ts
+++ Dating-DApp/matchmaking.ts
@@ -17,15 +17,21 @@ interface AcceptedMatches {
   acceptedMatchId: string;
 }
 
-async function makePotentialMatches(userId: string) {
-  const user = await prisma.profile.findUnique({ where: { id: userId } });
-  const potentialMatches = await prisma.profile.findMany({
-    where: {
-      gender: user.interestedIn,
-      interestedIn: user.gender,
-    },
-  });
-  return potentialMatches;
+async function addPotentialMatch(newUserId: string) {
+  const newUser = await prisma.profile.findUnique({ where: { id: newUserId } });
+  const existingUsers = await prisma.profile.findMany();
+
+  for (const user of existingUsers) {
+    if (user.gender === newUser.interestedIn && user.interestedIn === newUser.gender) {
+      // Add new user to the potentialMatches of the existing user
+      // This assumes that potentialMatches is a field in the Profile model
+      user.potentialMatches.push(newUserId);
+      await prisma.profile.update({
+        where: { id: user.id },
+        data: { potentialMatches: user.potentialMatches },
+      });
+    }
+  }
 }
 
 async function makeMatchRequests(userId: string, potentialMatchId: string) {
@@ -38,14 +44,26 @@ async function makeMatchRequests(userId: string, potentialMatchId: string) {
   return matchRequest;
 }
 
-async function acceptMatch(userId: string, requestedMatchId: string) {
-  const acceptedMatch = await prisma.like.create({
-    data: {
-      userId: userId,
-      acceptedMatchId: requestedMatchId,
-    },
-  });
-  return acceptedMatch;
+async function addAcceptedMatch(userId: string, likedUserId: string) {
+  const user = await prisma.profile.findUnique({ where: { id: userId } });
+  const likedUser = await prisma.profile.findUnique({ where: { id: likedUserId } });
+
+  if (user.potentialMatches.includes(likedUserId) && likedUser.potentialMatches.includes(userId)) {
+    // Add each other to the acceptedMatches
+    // This assumes that acceptedMatches is a field in the Profile model
+    user.acceptedMatches.push(likedUserId);
+    likedUser.acceptedMatches.push(userId);
+
+    await prisma.profile.update({
+      where: { id: userId },
+      data: { acceptedMatches: user.acceptedMatches },
+    });
+
+    await prisma.profile.update({
+      where: { id: likedUserId },
+      data: { acceptedMatches: likedUser.acceptedMatches },
+    });
+  }
 }
 
 async function rejectMatch(userId: string, requestedMatchId: string) {
