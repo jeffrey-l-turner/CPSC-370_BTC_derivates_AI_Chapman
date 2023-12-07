89ac2d77698cca4703fd25a4dfeb29074d7feedb
jlt <1573697+jeffrey-l-turner@users.noreply.github.com>
2023-12-07 11:03:54 -0800

    trying to deploy on vercel

--- Dating-DApp/final-project/src/lib/server/stytch.ts
+++ Dating-DApp/final-project/src/lib/server/stytch.ts
@@ -1,4 +1,4 @@
-import stytcher from 'stytch';
+import * as  stytcher  from 'stytch';
 
 export const stytch = new stytcher.Client({
 	project_id: 'project-test-619a9f52-eae0-44fe-913d-8adfd8fdb47f',
